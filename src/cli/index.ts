77cf44ebefc45c67906f65019402f3e94be6b8bc
Kohei Hasegawa <ameutau@gmail.com>
2022-09-27 21:23:35 +0900

    Fix description

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -19,7 +19,7 @@ yargs
         .option('repo', {
           type: 'string',
           describe:
-            'The name of the repository. The name is not case sensitive.',
+            'The name of the owner and repository. The name is not case sensitive. (e.g. owner/repo)',
           demandOption: true,
         })
         .option('base', {
